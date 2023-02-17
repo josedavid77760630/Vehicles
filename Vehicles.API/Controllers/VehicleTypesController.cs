9c97cc9b1d83cf0a1a3a71b67020a736cb6a3545
josedavidmamani77 <josedavidmamani77@gmail.com>
2023-02-17 17:11:44 -0400

    Vehicle type Completed

---
+++ Vehicles.API/Controllers/VehicleTypesController.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+using Vehicles.API.Data;
+using Vehicles.API.Data.Entities;
+
+namespace Vehicles.API.Controllers
+{
+    public class VehicleTypesController : Controller
+    {
+        private readonly DataContext _context;
+
+        public VehicleTypesController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.VehicleTypes.ToListAsync());
+        }
+
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(VehicleType vehicleType)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _ = _context.Add(vehicleType);
+                    _ = await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException ex)
+                {
+
+                    if (ex.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe este tipo de vehiculo.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, ex.InnerException.Message);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                }
+            }
+            return View(vehicleType);
+        }
+
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            VehicleType vehicleType = await _context.VehicleTypes.FindAsync(id);
+            return vehicleType == null ? NotFound() : View(vehicleType);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, VehicleType vehicleType)
+        {
+            if (id != vehicleType.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _ = _context.Update(vehicleType);
+                    _ = await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException ex)
+                {
+
+                    if (ex.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe este tipo de vehiculo.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, ex.InnerException.Message);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                }
+            }
+
+            return View(vehicleType);
+        }
+
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            VehicleType vehicleType = await _context.VehicleTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vehicleType == null)
+            {
+                return NotFound();
+            }
+
+            _ = _context.VehicleTypes.Remove(vehicleType);
+            _ = await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+    }
+}
---
+++ Vehicles.API/Data/Entities/VehicleType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vehicles.API.Data.Entities
+{
+    public class VehicleType
+    {
+        public int Id { get; set; }
+
+        [Display(Name ="Tipo de vehiculo")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(30, ErrorMessage = "El campo {0} no puede tener mas de {1} caracteres.")]
+        public string Description { get; set; }
+    }
+}
