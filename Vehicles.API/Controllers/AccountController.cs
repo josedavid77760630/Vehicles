50031c4f15a8ac907c6a779c80be502b59b22bc0
josedavidmamani77 <josedavidmamani77@gmail.com>
2023-02-24 21:51:03 -0400

    Selft registration user

--- Vehicles.API/Controllers/AccountController.cs
+++ Vehicles.API/Controllers/AccountController.cs
@@ -2,43 +2,47 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using Vehicles.API.Data;
+using Vehicles.API.Data.Entities;
 using Vehicles.API.Helpers;
 using Vehicles.API.Models;
+using Vehicles.Common.Enums;
 
 namespace Vehicles.API.Controllers
 {
-    public class AccountController: Controller
+    public class AccountController : Controller
     {
         private readonly IUserHelper _userlHelper;
+        private readonly DataContext _context;
+        private readonly ICombosHelper _combosHelper;
+        private readonly IBlobHelper _blobHelper;
 
-        public AccountController(IUserHelper userlHelper)
+        public AccountController(IUserHelper userlHelper, DataContext context, ICombosHelper combosHelper, IBlobHelper blobHelper)
         {
             _userlHelper = userlHelper;
+            _context = context;
+            _combosHelper = combosHelper;
+            _blobHelper = blobHelper;
         }
+
+
 
         public IActionResult Login()
         {
-            if (User.Identity.IsAuthenticated)
-            {
-                return RedirectToAction(nameof(Index), "Home");
-            }
-            return View(new LoginViewModel());
+            return User.Identity.IsAuthenticated ? RedirectToAction(nameof(Index), "Home") : View(new LoginViewModel());
         }
 
         [HttpPost]
-        public async Task<IActionResult> Login(LoginViewModel model) 
+        public async Task<IActionResult> Login(LoginViewModel model)
         {
             if (ModelState.IsValid)
             {
-                var result = await _userlHelper.LoginAsync(model);
+                Microsoft.AspNetCore.Identity.SignInResult result = await _userlHelper.LoginAsync(model);
                 if (result.Succeeded)
                 {
-                    if (Request.Query.Keys.Contains("ReturnUrl"))
-                    {
-                        return Redirect(Request.Query["ReturnUrl"].First());
-                    }
-
-                    return RedirectToAction(nameof(Index), "Home");
+                    return Request.Query.Keys.Contains("ReturnUrl")
+                        ? Redirect(Request.Query["ReturnUrl"].First())
+                        : RedirectToAction(nameof(Index), "Home");
                 }
                 ModelState.AddModelError(string.Empty, "Email o contraseña incorrectos");
             }
@@ -55,6 +59,57 @@ namespace Vehicles.API.Controllers
         {
             return View();
         }
+
+        public IActionResult Register()
+        {
+            AddUserViewModel model = new AddUserViewModel
+            {
+                DocumentTypes = _combosHelper.GetComboDocumentTypes()
+            };
+            return View(model);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(AddUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Guid imageId = Guid.Empty;
+
+                if(model.ImageFile != null)
+                {
+                    imageId = await _blobHelper.UploadBlobAsync(model.ImageFile, "users");
+                }
+                User user = await _userlHelper.AddUserAsync(model, imageId, UserType.User);
+                if (user == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Este correo ya está siendo usado por otro usuario.");
+                    model.DocumentTypes = _combosHelper.GetComboDocumentTypes();
+                    return View(model);
+                }
+
+                LoginViewModel loginViewModel = new LoginViewModel
+                {
+                    Password = model.Password,
+                    RememberMe = false,
+                    Username = model.Username
+                };
+
+                var result2 = await _userlHelper.LoginAsync(loginViewModel);
+                if (result2.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+            }
+            model.DocumentTypes = _combosHelper.GetComboDocumentTypes();
+            return View(model);
+        }
+
+
+
 
     }
 }
